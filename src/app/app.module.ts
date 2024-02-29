472d2b37227d55e82a309f089179af4d04eefa2a
Nihar Salunke <65081055+niharsalunke@users.noreply.github.com>
2024-02-29 08:00:03 +0530

    First Commit with Code Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomepageModule} from './homepage/homepage.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HomepageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
